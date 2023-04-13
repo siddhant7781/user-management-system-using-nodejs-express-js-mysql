2e8d1f35638880935cb6825820d189b5125c1ce3
siddhantPaudel <siddhantpaudel777@gmail.com>
2023-04-13 17:03:41 +0545

    form completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -39,9 +39,7 @@ exports.find = (req, res) => {
         if (err) throw err;
         console.log("database connected as ID ", connection.threadId);
 
-
         let searchTerm = req.body.search;
-
         //user the connection
         connection.query(`SELECT * FROM user WHERE first_name LIKE ? OR last_name LIKE ? `, ['%' + searchTerm + '%', '%' + searchTerm + '%'], (err, rows) => {
             //when done with connection,release it
@@ -55,14 +53,36 @@ exports.find = (req, res) => {
             // console.log('the data from user table: \n', rows);
         });
     });
+}
 
 
+exports.form = (req, res) => {
+
+    res.render('add-user');
 }
 
 //add new user
-exports.form = (req, res) => {
+exports.create = (req, res) => {
+    const { first_name, last_name, email, phone, comments } = req.body;
 
-    res.render('add-user')
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        console.log("database connected as ID ", connection.threadId);
+
+        let searchTerm = req.body.search;
+        //user the connection
+        connection.query('INSERT INTO user SET first_name=?, last_name=?,email=?,phone=?,comments=?', [first_name, last_name, email, phone, comments], (err, rows) => {
+            //when done with connection,release it
+            connection.release();
+
+            if (!err) {
+                res.render('add-user', { alert: `User Added Successfully. ` })
+            } else {
+                console.log(err);
+            }
+            // console.log('the data from user table: \n', rows);
+        });
+    });
 
 
 }
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -6,6 +6,7 @@ const userController = require('../controllers/userController');
 router.get('/', userController.view);
 router.post('/', userController.find);
 router.get('/adduser', userController.form);
+router.post('/adduser', userController.create);
 
 
 module.exports = router; 
